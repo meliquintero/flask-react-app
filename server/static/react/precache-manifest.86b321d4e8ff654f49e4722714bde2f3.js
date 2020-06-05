af0f4469c8bc0e7dccdf6fca3ec5f4a8c1a227f5
melissajimison <melissajimison@gmail.com>
2020-06-05 15:46:51 -0700

    run build

---
+++ server/static/react/precache-manifest.86b321d4e8ff654f49e4722714bde2f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34819fe024d4496c32cea796a4b99a2f",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "84d96506e2cf4278c365",
+    "url": "/static/react/css/main.8e96e706.chunk.css"
+  },
+  {
+    "revision": "f50d20218414043c7003",
+    "url": "/static/react/js/2.5f41e51e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.5f41e51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d96506e2cf4278c365",
+    "url": "/static/react/js/main.5e135203.chunk.js"
+  },
+  {
+    "revision": "2b2755d35b871c26a75b",
+    "url": "/static/react/js/runtime-main.bf13cb3c.js"
+  }
+]);
