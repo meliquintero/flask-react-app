1868b10555c4652870351769d97bbbfd5669ed9f
melissajimison <melissajimison@gmail.com>
2020-06-05 15:27:51 -0700

    set up server

--- server/main.py
+++ server/main.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, jsonify, request
 
 app = Flask (__name__)
+
 @app.route("/")
-
-def index():
-
-    return render_template("index.html",token="falsk reast")
-    # return "hello World"
-
-@app.route("/get-images")
-
-def get_images():
-    return send_file(loc_dir+res+".png", attachment_filename=res+".png", as_attachment=true)
-
+def my_index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-
     app.run(debug=True)
